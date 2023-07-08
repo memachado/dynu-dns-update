aac23cb27b902ad4e9fdbe6b019952d6bb03d29a
Miguel <miguelemachado@gmail.com>
2023-07-07 21:45:26 -0300

    External configurable DNS resolver

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,12 @@
 # Define a imagem base
-FROM python:3.8
+FROM 3.11.4-alpine3.17
 
-# Define o diretório de trabalho dentro do container
 WORKDIR /app
 
-# Copia o arquivo requirements.txt para o diretório de trabalho
-COPY requirements.txt .
-
-# Instala as dependências do projeto
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copia o código fonte do projeto para o diretório de trabalho
 COPY . .
 
-# Define o comando para iniciar a aplicação
-CMD [ "python", "app.py" ]
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN crontab crontab
+
+CMD ["crond", "-f"]
--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import json
-from requests import get, status_codes, codes, utils, exceptions
 import socket
-
-config = open('./config.json', 'r')
-conteudo = config.read()
-
-y = json.loads(conteudo)
-
-print(y["endpoint"])
+from requests import get, status_codes, codes, utils, exceptions
+import dns.edns
+import dns.message
+import dns.query
+import dns.resolver
 
 
-addr1 = socket.gethostbyname('google.com')
-addr2 = socket.gethostbyname('yahoo.com')
-print(addr1, addr2)
+def update_dns():
 
-response = get('https://api.ipify.org?format=json')
-data = response.json()
-public_ip = data['ip']
-print(public_ip)
+    config_text = open('./config.json', 'r').read()
+    config = json.loads(config_text)
+
+    public_ip = get('https://api.ipify.org?format=json').json()['ip']
+    print('public_ip', public_ip)
+    
+    for host in config['hosts']:
+        for alias in config['hosts'][host]['aliases']:
+            current_alias_ip = public_resolver(alias+"."+host)
+            print(alias, current_alias_ip[0].address)
+            if public_ip != current_alias_ip[0].address:
+                print("tem que atualizar")
+            else: 
+                print("não tem que atualizar")
+                
+def public_resolver(host_url):
+    aresolver = dns.resolver.Resolver()
+    aresolver.nameservers = ["8.8.8.8"]
+    return list(aresolver.resolve(host_url, "A"))
+
+if __name__ == "__main__":
+    update_dns()
+
