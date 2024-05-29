cf58994a30c9be7a8b7388f486ed3819aea40f8d
Miguel <20932346+memachado@users.noreply.github.com>
2024-05-29 16:31:40 -0300

    use hostsnames to update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dynu-dns-update
 
-Python script to check current host/alias IPs and update them when needed.
+Python script to check current host IPs and update them when needed.
 
 ## install
 
@@ -15,18 +15,14 @@ You can either run this on your computer or on a docker
                 "user": "",
                 "pass": ""
             },
-            "endpoint": "https://api.dynu.com/nic/update?hostname=_HOSTNAME_&alias=_ALIAS_&myip=_CURRENT_IP_",
-            "hosts": {
-                "somehost.com": {
-                    "aliases": [
-                        "alias1",
-                        "alias2",
-                        "alias3"
-                    ]
-                }
-            }
+            "endpoint": "https://api.dynu.com/nic/update?hostname=_HOSTNAME_&myip=_CURRENT_IP_",
+            "hosts": [
+                "somehost.com",
+                "anotherhost.com"
+            ]
         }
    ```
+
 3. Build the docker `docker build -t dynu-dns .`
 4. Run it
 
@@ -37,10 +33,6 @@ You need to provide user and password in the config.json. I prefer to use the sh
 You can choose any url from the reference documentation provided, but the one in the endpoint above works fine.
 
 You can choose how often the cron will execute the script editing the `cronjob` file and rebuilding the container
-
-Multiple hosts, with multiple alias can be included in the config.
-
-ONLY WORKS WITH ALIAS
 
 ## reference
 
--- app.py
+++ app.py
@@ -13,27 +13,28 @@ print('public_ip', public_ip)
 
 def check_dns():
     for host in config['hosts']:
-        for alias in config['hosts'][host]['aliases']:
-            try:
-                current_alias_ip = public_resolver(alias+"."+host)
-                print(alias+"."+host, current_alias_ip[0].address)
-                if public_ip != current_alias_ip[0].address:
-                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    print('updated at ', current_time)
-                    update_dns_alias(host, alias)
-            except Exception as e:
-                print(alias+"."+host, "not found")
+        try:
+            current_alias_ip = public_resolver(host)
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            if public_ip != current_alias_ip[0].address:
+                print(current_time, 'missmatching ip at ' ,host, current_alias_ip[0].address)
+                update_dns_alias(host)
+        except Exception as e:
+            print(host, "not found")
 def public_resolver(host_url):
     aresolver = dns.resolver.Resolver()
     aresolver.nameservers = ["8.8.8.8"]
     return list(aresolver.resolve(host_url, "A"))
 
-def update_dns_alias(host_name, alias_name):
+def update_dns_alias(host_name):
     url = config['endpoint'].replace("_HOSTNAME_", host_name)
-    url = url.replace("_ALIAS_", alias_name)
     url = url.replace("_CURRENT_IP_", public_ip)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(current_time, 'updating ', host_name)
 
     response = get(url, auth=(config['credentials']['user'], config['credentials']['pass']))
+
     print(response)
 
 if __name__ == "__main__":
